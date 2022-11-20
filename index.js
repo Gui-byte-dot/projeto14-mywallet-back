78adf1ccac5630967b347d3de164da59e20de32d
guilherme <grgomes82@gmail.com>
2022-11-20 00:16:23 -0300

    first commit

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ app.post("/sign-in", async (req, res) => {
     }
   });
   app.listen(5000, () => {
-    console.log("Server is listening on port 5000.")
+    console.log("Server is listening on port 3000.")
   });
 
 
